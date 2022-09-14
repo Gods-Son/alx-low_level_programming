d7c56a1765215a3ab990aa19bb4b453c798a8272
Gods-Son <kingijioma@gmail.com>
2022-09-13 22:57:37 -0700

    my FNL

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,14 +8,13 @@
  * -1 and prints - if n is less than zero
  */
 int print_sign(int n)
-
 {
 
 	if (n > 0)
 	{
 		_putchor('+');
 		return (1);
-	}else if (n == 0)
+	} else if (n == 0)
 	{
 		_putchar(48);
 		return (0);
