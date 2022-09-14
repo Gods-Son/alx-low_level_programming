1bb48f5eaa5ee9124fe13a8e58f2cb2f9b633f24
Gods-Son <kingijioma@gmail.com>
2022-09-13 21:34:29 -0700

    my FNL

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,10 +4,8 @@
  * Main - prints _putchar, followed by a new line
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-
 _putchar('_');
 _putchar('p');
 _putchar('u');
@@ -17,6 +15,7 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
-										return (0);
+
+return (0);
 
 }
