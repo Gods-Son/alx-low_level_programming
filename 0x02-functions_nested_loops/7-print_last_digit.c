d2aa8bc24e8eb69872df5f07a658e073c151a303
Gods-Son <kingijioma@gmail.com>
2022-09-14 22:16:04 -0700

    my FNL

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
 /**
- *
  * print_last_digit - prints the last digit of a number
  * @n: the int to extract the last digit from
  * Return: value of the last digit
@@ -22,4 +21,3 @@ int print_last_digit(int n)
 
 	return (a);
 }
-
