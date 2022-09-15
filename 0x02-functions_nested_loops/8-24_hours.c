91b040fa7d9b9edb5d35406ffe3880e7d8c0c185
Gods-Son <kingijioma@gmail.com>
2022-09-14 22:30:11 -0700

    my FNL

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,13 +7,12 @@
 void jack_bauer(void)
 {
 	int i, j;
-
 	i = 0;
 
 	while (i < 24)
 	{
 		j = 0;
-		while (J < 60)
+		while (j < 60)
 		{
 			_putchar((i / 10) + '0');
 			_putchar((i % 10) + '0');
