da9528f9d03d388b7f104fa72b004cf83f4cc43a
Gods-Son <kingijioma@gmail.com>
2022-09-14 22:38:36 -0700

    my FNL

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,7 +13,7 @@ void times_table(void)
 		for (j = 0; j < 10; j++)
 		{
 			k = j * i;
-			if (J == 0)
+			if (j == 0)
 			{
 				_putchar(k + '0');
 			}
@@ -33,5 +33,5 @@ void times_table(void)
 			}
 		}
 		_putchar('\n');
-	}		
+	}
 }
