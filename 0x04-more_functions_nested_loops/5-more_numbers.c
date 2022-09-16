3f60d4c547b12ae8acacc751fb684c606f8c7831
Gods-Son <kingijioma@gmail.com>
2022-09-16 04:59:23 -0700

    my FNL

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,4 +1,4 @@
-#include"main.h"
+#include "main.h"
 
 /**
  * more_numbers - print 0 - 14 ten times and you
@@ -9,6 +9,7 @@
 void more_numbers(void)
 {		
 int num, row, count;
+	
 	for (row = 1; row <= 10; ++row)
 	{
 	for (count = 0; count <= 14; ++count)
