3d694001e1fbeae02c4de5a6fc5f35508579b83a
Gods-Son <kingijioma@gmail.com>
2022-09-16 21:32:04 -0700

    my FNL 2

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -9,16 +9,15 @@
 double _sqrt(double x)
 {
 float sqrt, tmp;
-
 sqrt = x / 2;
 tmp = 0;
 
 while (sqrt != tmp)
 {
-	tmp = sqrt;
-	sqrt = (x / tmp + tmp) / 2;
+tmp = sqrt;
+sqrt = (x / tmp + tmp) / 2;
 }
-	return (sqrt);
+return (sqrt);
 }
 
 /**
@@ -29,22 +28,24 @@ while (sqrt != tmp)
 
 void largest_prime_factor (long int num)
 {
-	int prmNu, largest;
+int prmNu, largest;
 
-	/* first divide with the smallest prime number (two) */
-	while (num % 2 == 0)
-		num = num / 2;
-	/* num must be cold do we proceed to the next prime number (plus two) */
-	for (prmNu = 3; prmNu <= _sqrt(num); prmNu += 2)
-	{
-		while (num % prmNu == 0)
-		{
-			num / prmNu:
-			largest = prmNu;
-		}
-	}
+/* first divide with the smallest prime number (two) */
+while (num % 2 == 0)
+num = num / 2;
+/* num must be cold do we proceed to the next prime number (plus two) */
+
+for (prmNu = 3; prmNu <= _sqrt(num); prmNu += 2)
+{
+while (num % prmNu == 0)
+{
+num / prmNu:
+largest = prmNu;
+}
+}
+
 if (num > 2)
-	largest = num;
+largest = num;
 printf("%d\n", largest);
 }
 
