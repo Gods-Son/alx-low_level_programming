45d702fe85bc64fe96c72ca6c81d930b4d51a1a6
Gods-Son <kingijioma@gmail.com>
2022-09-20 17:10:25 -0700

    pointers & more

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/** 
+/**
  * print_rev - print a string in reverse followed by a new line
  * @s: string to be reversed
  */
@@ -8,7 +8,6 @@
 void print_rev(char *s)
 {
 int i = 0;
-
 while (*(s + i) != '\0')
 i += 1;
 i -= 1;
