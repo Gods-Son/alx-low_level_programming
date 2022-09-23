bb32a22e390595a3e825650542d08de1011f99e2
Gods-Son <kingijioma@gmail.com>
2022-09-22 22:14:44 -0700

    more pointers

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,7 +2,7 @@
 
 /**
  * cap_string - capitalizes everey word of a string
- * s: string to modify
+ * @s: string to modify
  * Return: the resulting string
  */
 
@@ -15,7 +15,6 @@ for (i = 0; s[i] != '\0'; i++)
 {
 if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
 s[i] -= 32;
-
 for (j = 0; j < 13; j++)
 {
 if (s[i] == spe[j])
