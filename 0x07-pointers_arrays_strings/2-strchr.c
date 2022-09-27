d26ffecf1a4e6f1a87ee63d6f16a8faee84139ae
Gods-Son <kingijioma@gmail.com>
2022-09-27 16:07:40 -0700

    second P_A_S

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * *_strchr - locates a character in a string
+ * _strchr - locates a character in a string
  * @s: string to search
  * @c: char to find
  * Return: a pointer to the first occurrence of the character
@@ -12,7 +12,6 @@
 char *_strchr(char *s, char c)
 {
 int a;
-
 while (1)
 {
 a = *s++;
