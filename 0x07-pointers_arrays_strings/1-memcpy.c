dd016c43cf38f4323e5f02555a9f3da20bbbd0c2
Gods-Son <kingijioma@gmail.com>
2022-09-28 21:57:00 -0700

    second P_A_S

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _memcpy - copies memory area
+ * char *_memcpy - copies memory area
  * @dest: destination memory area
  * @src: memory area to copy from
  * @n: number of bytes to copy
@@ -10,12 +10,12 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int size = n;
-	if (size > 0)
-	{
-	int i;
-	for (i = 0; i < size; i++)
-	dest[i] = src[i];
-	}
-	return (dest);
+int size = n;
+if (size > 0)
+{
+int i;
+for (i = 0; i < size; i++)
+dest[i] = src[i];
+}
+return (dest);
 }
