d269e838df142c3e80c698b17d9683a7fbcab077
Gods-Son <kingijioma@gmail.com>
2022-09-28 21:34:46 -0700

    second P_A_S

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * _memset - fills memory with a constant byte
- * 
+ * _memset - fills memory with a constant byte.
  * @s: pointer to memory area to fill
  * @n: the number of bytes to fill
  * @b: constant byte
