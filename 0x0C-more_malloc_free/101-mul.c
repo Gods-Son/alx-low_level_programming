076ed9aa714dc6836c9d6326782c269fe64fc070
Gods-Son <kingijioma@gmail.com>
2022-10-06 21:39:11 -0700

    input updated

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -8,6 +8,7 @@
  * @argv: arguments
  * Return: 0 success
  */
+
 int main(int argc, char *argv[])
 {
 unsigned long mul;
@@ -25,7 +26,7 @@ int i, j;
 	exit(98); }
 	}
 	}
-	mul = atol(argv[1]) *  atol(argv[2]);
+	mul = atol(argv[1]) * atol(argv[2]);
 	printf("%lu\n", mul);
 	return (0);
 }
