2142267c295f910b89388811921f5c81faa9784f
Gods-Son <kingijioma@gmail.com>
2022-10-06 21:11:59 -0700

    input

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,11 +2,9 @@
 
 /**
  * string_nconcat - concatenates two strings
- *
  * @s1: first string
  * @s2: second string
  * @n: the number of bytes to include of @s2
- * 
  * Return: newly allocated space in memory;
  * NULL if the function fails
  */
@@ -31,18 +29,14 @@ while (s2[j])
 	j++;
 if (j > n)
 	j = n;
-
 len = i + j;
-
 ar = malloc(sizeof(char) * (len + 1));
 if (ar == NULL)
 	return (NULL);
-
 for (p = 0; p < i; p++)
 	ar[k++] = s1[p];
 for (m = 0; m < j; m++)
 	ar[k++] = s2[m];
-
 ar[k] = '\0';
 return (ar);
 }
