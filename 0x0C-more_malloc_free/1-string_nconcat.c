70c06eed789c6f8a113535b610d8df2865e62fbb
Gods-Son <kingijioma@gmail.com>
2022-10-06 20:57:25 -0700

    input

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,6 +2,7 @@
 
 /**
  * string_nconcat - concatenates two strings
+ *
  * @s1: first string
  * @s2: second string
  * @n: the number of bytes to include of @s2
@@ -12,38 +13,36 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *ar;
-	unsigned int i = 0;
-	unsigned int j = 0;
-	unsigned int m;
-	unsigned int p;
-	unsigned int k = 0;
-	unsigned int len;
+char *ar;
+unsigned int i = 0;
+unsigned int j = 0;
+unsigned int m;
+unsigned int p;
+unsigned int k = 0;
+unsigned int len;
 
-	if (s1 == NULL)
-
-s1 = "";
+if (s1 == NULL)
+	s1 = "";
 if (s2 == NULL)
 	s2 = "";
-	while (s1[i])
+while (s1[i])
 	i++;
-	while (s2[j])
+while (s2[j])
 	j++;
-	if (j > n)
+if (j > n)
 	j = n;
 
-	len = i + j;
+len = i + j;
 
-	ar = malloc(sizeof(char) * (len + 1));
-	if (ar == NULL)
+ar = malloc(sizeof(char) * (len + 1));
+if (ar == NULL)
 	return (NULL);
 
-	for (p = 0; p < i; p++)
+for (p = 0; p < i; p++)
 	ar[k++] = s1[p];
-	for (m = 0; m < j; m++)
+for (m = 0; m < j; m++)
 	ar[k++] = s2[m];
 
-	ar[k] = '\0';
-	return (ar);
-	}
-
+ar[k] = '\0';
+return (ar);
+}
