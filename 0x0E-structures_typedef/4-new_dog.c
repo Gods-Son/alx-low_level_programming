828d063df4be83cc88f4002e84583b5e2cd73f1e
Gods-Son <kingijioma@gmail.com>
2022-10-10 21:57:20 -0700

    struct files

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,35 +15,30 @@ void fillMem(char *str, int strLen, char *dest);
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *n_dog;
-	int nameLen, ownerLen;
-	n_dog = malloc(sizeof(dog_t));
-
-	if (n_dog == NULL)
-		return (NULL);
-	nameLen = _strLen(name);
-	n_dog->name = malloc(sizeof(char) * nameLen + 1);
-
-	if (n_dog->name == NULL)
-	{
-		free(n_dog);
-		return (NULL);
-	}
-
-	fillMem(name, nameLen, n_dog->name);
-	ownerLen = _strLen(owner);
-	n_dog->owner = malloc(sizeof(char) * ownerLen + 1);
-
-	if (n_dog->owner == NULL)
-	{
-		free(n_dog);
-		free(n_dog->name);
-		return (NULL);
-	}
-
-	fillMem(owner, ownerLen, n_dog->owner);
-	n_dog->age = age;
-	return (n_dog);
+dog_t *n_dog;
+int nameLen, ownerLen;
+n_dog = malloc(sizeof(dog_t));
+if (n_dog == NULL)
+return (NULL);
+nameLen = _strLen(name);
+n_dog->name = malloc(sizeof(char) * nameLen + 1);
+if (n_dog->name == NULL)
+{
+free(n_dog);
+return (NULL);
+}
+fillMem(name, nameLen, n_dog->name);
+ownerLen = _strLen(owner);
+n_dog->owner = malloc(sizeof(char) * ownerLen + 1);
+if (n_dog->owner == NULL)
+{
+free(n_dog);
+free(n_dog->name);
+return (NULL);
+}
+fillMem(owner, ownerLen, n_dog->owner);
+n_dog->age = age;
+return (n_dog);
 }
 
 /**
@@ -56,15 +51,15 @@ int _strLen(char *str)
 {
 int i = 0;
 while (str[i])
-	i++;
+i++;
 return (i);
 }
 
 /**
- *  fillMem - Copy string literal to allocated memory
- *  @str: String literal
- *  @strLen: @str length
- *  @dest: The allocated memory
+ * fillMem - Copy string literal to allocated memory
+ * @str: String literal
+ * @strLen: @str length
+ * @dest: The allocated memory
  */
 
 void fillMem(char *str, int strLen, char *dest)
