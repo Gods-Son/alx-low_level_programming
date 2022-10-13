6274bd35cb665bd4cae0282c24a76b39b0d55862
Gods-Son <kingijioma@gmail.com>
2022-10-12 18:01:27 -0700

    update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,11 +5,13 @@
  * @name: name to print
  * @f: function to print with
  *
+ * Description: prints a name using function pointers
+ *
  * Return: void
  */
 
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
-	f(name);
+		f(name);
 }
