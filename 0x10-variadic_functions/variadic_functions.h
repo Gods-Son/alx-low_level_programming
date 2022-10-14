29c873aa2f5954b8f7111e2260f1330c6fe161db
Gods-Son <kingijioma@gmail.com>
2022-10-13 22:53:41 -0700

    variadic_functions h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,10 +3,15 @@
 #include <stdarg.h>
 
 int _putchar(char c);
+
 int sum_them_all(const unsigned int n, ...);
+
 void print_numbers(const char *separator, const unsigned int n, ...);
+
 void print_strings(const char *separator, const unsigned int n, ...);
+
 void print_all(const char * const format, ...);
+
 
 /**
  * struct format_struct - format-type class
@@ -21,6 +26,7 @@ struct format_struct
 char format;
 void (*fmt_print_func)(va_list *arg_list);
 };
+
 typedef struct format_struct format_struct_ptr;
 
 #endif
