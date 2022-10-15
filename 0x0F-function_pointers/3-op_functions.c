c477b8bec77c093a28e04913951f0e55da21c5ef
Gods-Son <kingijioma@gmail.com>
2022-10-14 19:42:26 -0700

    create 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -12,7 +12,7 @@
 
 int op_add(int a, int b)
 {
-	return (a + b);
+return (a + b);
 }
 
 /**
@@ -27,7 +27,7 @@ int op_add(int a, int b)
 
 int op_sub(int a, int b)
 {
-	return (a - b);
+return (a - b);
 }
 
 /**
@@ -42,7 +42,7 @@ int op_sub(int a, int b)
 
 int op_mul(int a, int b)
 {
-	return (a * b);
+return (a * b);
 }
 
 /**
@@ -59,8 +59,8 @@ int op_div(int a, int b)
 {
 	if (b == 0)
 	{
-		printf("Error\n");
-		exit(100);
+	printf("Error\n");
+	exit(100);
 	}
 	return (a / b);
 }
@@ -77,10 +77,10 @@ int op_div(int a, int b)
 
 int op_mod(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-	return (a % b);
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
+return (a % b);
 }
