5e2c30d52ce3b24e3346c63cd44f2a4c0d888cd4
Gods-Son <kingijioma@gmail.com>
2022-10-23 00:41:21 -0700

    2-add_node.c file

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -4,15 +4,14 @@
 #include "lists.h"
 
 /**
- * add_node - Adds a new node at the beginning of list_t list
- * @new_head: Address of node
- * @str: New node to add
- * Return: Address of new element, NULL if it fails
- */
+* add_node - Adds a new node at the beginning of list_t list
+* @new_head: Address of node
+* @str: New node to add
+* Return: Address of new element, NULL if it fails
+*/
 
 list_t *add_node(list_t **head, const char *str)
 {
-
 list_t *new_node_ptr = malloc(sizeof(list_t));
 
 new_head = malloc(sizeof(list_t));
