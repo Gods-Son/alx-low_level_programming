92322c764aa00f72a4a8ed1edef5bd438ecc3318
Gods-Son <kingijioma@gmail.com>
2022-10-25 15:35:51 -0700

    update Create 6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * Pop_listint - Deletes the node of a listint_t list
+ * pop_listint - Deletes the node of a listint_t list
  * and return the head node's data @n
  * @head: Double pointer the head node
  * Return: The head node data n, 0 if empty
@@ -10,16 +10,16 @@
 
 int pop_listint(listint_t **head)
 {
-	listint_t *node_to_delete;
-	int n;
+listint_t *node_to_delete;
+int n;
 
-	if (*head == NULL)
-		return (0);
+if (*head == NULL)
+return (0);
 
-	node_to_delete = *head;
-	*head = node_to_delete->next;
-	n = node_to_delete->n;
-	free(node_to_delete);
+node_to_delete = *head;
+*head = node_to_delete->next;
+n = node_to_delete->n;
+free(node_to_delete);
 
-	return (n);
+return (n);
 }
